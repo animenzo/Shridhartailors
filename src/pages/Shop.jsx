38941c04fda743fbe7198ae523a3c3c50f7f0fad
piyush tailor <151143121+animenzo@users.noreply.github.com>
2025-05-21 19:34:49 +0530

    Create missing Shop page component

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className="min-h-screen pt-20">
+      <h1 className="text-4xl font-bold text-center">Shop</h1>
+    </div>
+  )
+}
+
+export default Shop
